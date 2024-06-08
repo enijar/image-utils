8bf219d165afb4b59af235abd79fc4e0fad8f4da
Enijar <TheEnijar@gmail.com>
2024-06-08 18:55:42 +0100

    chore: project set up

---
+++ README.md
@@ -0,0 +1,17 @@
+# Image Utils
+
+Utilities for common image needs.
+
+### Getting Started
+
+```shell
+npm install
+npm start
+```
+
+### Production Build
+
+```shell
+npm install
+npm run build
+```
---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as Style from "@/components/app/app.style.ts";
+
+export default function App() {
+  return (
+    <>
+      <Style.AppReset />
+      <h1>App</h1>
+    </>
+  );
+}
